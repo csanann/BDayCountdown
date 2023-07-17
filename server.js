e4854ec740be91076795e5d898325587c5230f9f
csanann <csanann@gmail.com>
2023-07-17 22:52:53 +0100

    Refactor, inspect error resolving in process

--- server.js
+++ server.js
@@ -1,23 +1,40 @@
 // file: server.js
 
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const { calculateRemainingDays } = require('./src/calculateDays');
+const { isValidInput } = require('./src/main');
 
 const app = express();
+const port = 3000();
 
+app.use(express.json());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/birthday', (reg, res) => {
+app.post('/calculate', (req, res) => {
+  const { name, dob } = req.body;
+
+  if (!isValidInput(name, dob)) {
+    res.status(400).json({ error: 'Invalid input' });
+    return;
+  }
+
   try {
-    const dateOfBirth = req.query.dob;
-    const remainingDays = calculateRemainingDays(dateOfBirth);
+    const remainingDays = calculateRemainingDays(dob);
     res.json({ remainingDays });
   } catch (error) {
     res.status(400).json({ error: error.toString() });
   }
 });
 
+app.get('/result', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'result.html'));
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+//since this is a simple app, we hence have to set routes here
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 // file: src/main.js
 
 function isValidInput(name, dob) {
-  if (typeof name !== 'string' || name.trim() === '' || !/^(\d{2})\/(\d{2})\/(\d{4})$/.test(dob)) {
-      return false;
+  // if (typeof name !== 'string' || name.trim() === '' || !/^(\d{2})\/(\d{2})\/(\d{4})$/.test(dob)) {
+  if (typeof name !== 'string' || name.trim() === '') {  
+    return false;
+  }
+
+  const dateParts = dob.split('-');
+  if (dateParts.length !== 3) {
+    return false;
+  }
+
+  const year = parseInt(dateParts[0]);
+  const month = parseInt(dateParts[1]);
+  const day = parseInt(dateParts[2]);
+
+  const date = new Date(year, month -1, day);
+  if (!(date && date.getFullYear() === year && date.getMonth() === month - 1 && date.getDate() === day)) {
+    return false;
   }
 
   return true;
