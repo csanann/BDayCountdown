2daad51d82733c4adcee81a11c583d16a605dbd9
csanann <csanann@gmail.com>
2023-07-17 22:48:58 +0100

    Unit Test passing

--- tests/main.test.js
+++ tests/main.test.js
@@ -3,10 +3,10 @@
 const { isValidInput } = require('../src/main');
 
 test('Validates user input', () => {
-  expect(isValidInput('Ann San', '12/05/1989')).toBe(true);
-  expect(isValidInput('Ann San', '05/05/1989')).toBe(true);
+  expect(isValidInput('Ann San', '1989-05-12')).toBe(true);
+  expect(isValidInput('Ann San', '1989-05-05')).toBe(true);
   expect(isValidInput('Ann San', 'random string')).toBe(false);
-  expect(isValidInput(12345, '12/05/1989')).toBe(false);
-  expect(isValidInput('', '12/05/1989')).toBe(false);
+  expect(isValidInput(12345, '1989-05-12')).toBe(false);
+  expect(isValidInput('', '1989-05-12')).toBe(false);
   expect(isValidInput('Ann San', '')).toBe(false);
 });
